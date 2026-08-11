7ea12608abfd98617854103bdad45b6a78583e02
Sougandhika <144715806+sougandhikasatheesh@users.noreply.github.com>
2026-08-11 21:50:49 +0530

    Fix typo in originalname property for multer

--- backend/config/multer.js
+++ backend/config/multer.js
@@ -4,7 +4,7 @@ const path=require("path");
 const storage=multer.diskStorage({
     destination:(req,file,cb)=> cb(null,"uploads/"),
     filename:(req,file,cb)=>{
-        cb(null,Date.now()+path.extname(file.orginalname));
+        cb(null,Date.now()+path.extname(file.originalname));
     }
 });
 
